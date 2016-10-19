f38b4b9a28c25ddfece06ae220c2fcbaef37d8fb
sultan2013 <sultanmm2012@gmail.com>
2016-10-19 23:27:21 +0300

    firebase has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {initializeApp,database} from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
   about = 'this app is built to help be in touch with family and relatives';
-}
+
+  constructor(){
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyBiYzysaXI6LFjCNGonk7IPmXyIX66Lu2I",
+      authDomain: "relativescall.firebaseapp.com",
+      databaseURL: "https://relativescall.firebaseio.com",
+      storageBucket: "relativescall.appspot.com",
+      messagingSenderId: "627182190390"
+    };
+    initializeApp(config);
+
+    var root = database().ref();
+    root.on('value', function(snap){
+      console.log(snap.val)});
+    }
+
+  }
